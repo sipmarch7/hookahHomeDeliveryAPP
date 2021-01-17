ec64b26b34483319d17b6cf61fcc704f65ffb57e
Spyridon Peppas <spyridonpeppas@Spyridons-MacBook-Pro.local>
2021-01-17 23:11:57 +0200

    add new flavors and email confirmtions

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../model/db.mysql');
 const checkAuthentication = require('../passport/checkAuthentication');
+const nodemailer = require('nodemailer');
 
 router.get('/adminOrders', checkAuthentication.checkAuthenticated, (req, res) => {
     let sql0 = "SELECT * FROM tbl_orders INNER JOIN tbl_users ON tbl_orders.user_id=tbl_users.user_id ORDER BY date DESC, order_id DESC;"
@@ -17,6 +18,9 @@ router.get('/adminOrders', checkAuthentication.checkAuthenticated, (req, res) =>
             }
             var totalOrdersCompleted = results1.length
             if (req.user.isAdmin){
+                for (i in results0){
+                    results0[i].date = dateForEurope(results0[i].date)
+                }
                 res.render('adminOrders',{user: req.user, result: results0, totalEarnings: totalEarnings, totalOrdersCompleted: totalOrdersCompleted});
                 return
             }
@@ -31,6 +35,54 @@ router.post('/adminOrders/cancelOrder', checkAuthentication.checkAuthenticated, 
         var sql = "UPDATE tbl_users SET numberOfOrders = numberOfOrders - 1 WHERE user_id="+req.body.user_id
         let query = conn.query(sql, (err, result) => {
             if(err) throw err;
+            // email EIMAIFEAKFKEFLAEF EMAILEMAIL EMAIL EMAIL EMAIL EMAIL EMAILEMAIL
+            var smtpTrans = nodemailer.createTransport({
+                service: 'gmail',
+                port: 465,
+                secure: true,
+                auth: {
+                  user: process.env.GMAIL_USER,
+                  pass: process.env.GMAIL_PASS
+                }
+              });
+              var mailOptions = {
+                from: 'sender',
+                to: 'panweq@gmail.com', //shishahublc@gmail.com //sipmarch7@hotmail.com
+                subject: 'CANCELED Shishahub Order '+req.user.firstname,
+                text: 'Δυστυχώς η παραγγελία σου με αριθμό 32'+req.body.order_id+'47 ακυρώθηκε.'+
+                  '\nΖητούμενη συγνώμη αλλά δεν μπορούμε να σε εξυπηρετήσουμε αυτή τη χρονική στιγμή που έχεις διαλέξει.'+
+                  '\nΕάν μπορούμε να σε εξυπηρετήσουμε κάποια άλλη ώρα για την ίδια ημέρα που έχεις διαλέξει,'+
+                  '\nθα επικοινωνήσουμε άμεσα μαζί σου, στο τηλέφωνο που έχεισ δηλώσει.'+
+                  '\nΕάν δεν είναι ούτε αυτό εφικτό μπορείς να προσπαθήσεις ξανά κάποια άλλη μέρα.'+
+                  '\nΣε ευχαριστούμε για την προτίμησή σου στο Shishahub.'+
+                  '\n\nORDER DETAILS'+
+                  '\n——————————————CANCELED————————————————'+
+                  '\n Order ID  : '+'32'+ req.body.order_id+'47'+" "+" No."+req.user.numberOfOrders+
+                  '\n Date       : '+ req.body.date+
+                  '\n Time       : '+ req.body.time+
+                  '\n Duration  : '+ req.body.duration+
+                  '\n Flavors    : '+ req.body.flavors+
+                  '\n Price      : '+ price(req.body.duration, req.user.outOfLoutraki, req.user.numberOfOrders)+
+                  '\n Other Address : '+ req.body.textArea+
+                  '\n\nACCOUNT DETAILS'+
+                  '\n——————————————————————————————'+
+                  '\n Name       : '+ req.user.firstname+" "+req.user.lastname+
+                  '\n Email      : '+ req.user.email+
+                  '\n Telephone : '+ req.user.telephone+
+                  '\n Address    : '+ req.user.address+
+                  '\n Floor        : '+ req.user.floor+
+                  '\n City         : '+ req.user.city+
+                  '\n Postal      : '+ req.user.postal
+              };
+        
+              smtpTrans.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+              });
+              // email EIMAIFEAKFKEFLAEF EMAILEMAIL EMAIL EMAIL EMAIL EMAIL EMAILEMAIL
             res.redirect('/admin/adminOrders')
         })
     })
@@ -39,6 +91,52 @@ router.post('/adminOrders/cancelOrder', checkAuthentication.checkAuthenticated, 
 router.post('/adminOrders/acceptOrder', checkAuthentication.checkAuthenticated, (req,res)=>{
     let sql = "UPDATE tbl_orders SET status='accepted', accepted=1, admin='"+req.user.firstname+"' WHERE order_id="+req.body.order_id;
     let query = conn.query(sql, (err,results) => {
+        // email EIMAIFEAKFKEFLAEF EMAILEMAIL EMAIL EMAIL EMAIL EMAIL EMAILEMAIL
+        var smtpTrans = nodemailer.createTransport({
+            service: 'gmail',
+            port: 465,
+            secure: true,
+            auth: {
+              user: process.env.GMAIL_USER,
+              pass: process.env.GMAIL_PASS
+            }
+          });
+          var mailOptions = {
+            from: 'sender',
+            to: 'panweq@gmail.com', //shishahublc@gmail.com //sipmarch7@hotmail.com
+            subject: 'ACCEPTED Shishahub Order '+req.user.firstname,
+            text: 'H παραγγελία σου με αριθμό 32'+req.body.order_id+'47 έγινε δεκτή.'+
+              '\nΚατά την ώρα που επέλεξες ο ειδικός μας θα σε καλέσει, στο τηλέφωνο που έχεις δηλώσει,'+
+              '\nώστε να σε ειδοποιήσει ότι έχει φτάσει ή ότι είναι πολύ κοντά στον χώρο σου.'+
+              '\nΣε ευχαριστούμε για την προτίμησή σου στο Shishahub.'+
+              '\n\nORDER DETAILS'+
+              '\n——————————————CANCELED————————————————'+
+              '\n Order ID  : '+'32'+ req.body.order_id+'47'+" "+" No."+req.user.numberOfOrders+
+              '\n Date       : '+ req.body.date+
+              '\n Time       : '+ req.body.time+
+              '\n Duration  : '+ req.body.duration+
+              '\n Flavors    : '+ req.body.flavors+
+              '\n Price      : '+ price(req.body.duration, req.user.outOfLoutraki, req.user.numberOfOrders)+
+              '\n Other Address : '+ req.body.textArea+
+              '\n\nACCOUNT DETAILS'+
+              '\n——————————————————————————————'+
+              '\n Name       : '+ req.user.firstname+" "+req.user.lastname+
+              '\n Email      : '+ req.user.email+
+              '\n Telephone : '+ req.user.telephone+
+              '\n Address    : '+ req.user.address+
+              '\n Floor        : '+ req.user.floor+
+              '\n City         : '+ req.user.city+
+              '\n Postal      : '+ req.user.postal
+          };
+    
+          smtpTrans.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+          // email EIMAIFEAKFKEFLAEF EMAILEMAIL EMAIL EMAIL EMAIL EMAIL EMAILEMAIL
         res.redirect('/admin/adminOrders')
     })
 })
@@ -61,3 +159,35 @@ router.post('/adminOrders/deliveredOrder', checkAuthentication.checkAuthenticate
 
 
 module.exports = router;
+
+function dateForEurope(date){
+    var dd = date.slice(8)
+    var mm = date.slice(5,7)
+    var yy = date.slice(0,4)
+    return dd+"-"+mm+"-"+yy 
+  }
+
+function price(duration, outOfLoutraki, numberOfOrders){
+    if (duration==='4'){
+      if (outOfLoutraki && numberOfOrders){
+        return "30"
+      }else if ((outOfLoutraki && !numberOfOrders) || (!outOfLoutraki && numberOfOrders)){
+        return "25"
+      }
+      return "20"
+    }else if (duration==='6'){
+      if (outOfLoutraki && numberOfOrders){
+        return "40"
+      }else if ((outOfLoutraki && !numberOfOrders) || (!outOfLoutraki && numberOfOrders)){
+        return "35"
+      }
+      return "30"
+    }else{
+      if (outOfLoutraki && numberOfOrders){
+        return "55"
+      }else if ((outOfLoutraki && !numberOfOrders) || (!outOfLoutraki && numberOfOrders)){
+        return "50"
+      }
+      return "45"
+    }
+  }
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../model/db.mysql');
 const checkAuthentication = require('../passport/checkAuthentication');
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 
 router.get('/', (req, res) => {
   res.render('product', { user: req.user});
@@ -41,15 +41,15 @@ router.post('/orderPreview', checkAuthentication.checkAuthenticated,  (req, res)
     status : "pending", otherAddress: req.body.textArea
   }
   let sql = "INSERT INTO tbl_orders SET ?";
-  let query = conn.query(sql, data,(err, result) => {
+  let query = conn.query(sql, data,(err, result0) => {
     if(err) throw err;
     res.redirect('orderConfirmation');
     
     //update number of orders at tbl_users 
     var sql = "UPDATE tbl_users SET numberOfOrders = numberOfOrders + 1 WHERE user_id="+req.user.user_id
-    let query = conn.query(sql, (err, result) => {
+    let query = conn.query(sql, (err, result1) => {
       if(err) throw err;
-    
+      var numOfOrders = req.user.numberOfOrders+1
       // email EIMAIFEAKFKEFLAEF EMAILEMAIL EMAIL EMAIL EMAIL EMAIL EMAILEMAIL
       var smtpTrans = nodemailer.createTransport({
         service: 'gmail',
@@ -62,13 +62,13 @@ router.post('/orderPreview', checkAuthentication.checkAuthenticated,  (req, res)
       });
       var mailOptions = {
         from: 'sender',
-        to: 'shishahublc@gmail.com', //shishahublc@gmail.com //sipmarch7@hotmail.com
+        to: 'panweq@gmail.com', //shishahublc@gmail.com //sipmarch7@hotmail.com
         subject: 'NEW ORDER From: '+req.user.firstname+" "+req.user.lastname,
         text: 'DAY : '+req.body.date+' at '+req.body.time+
           '\n——————————————————————————————'+
           '\n\nORDER DETAILS'+
           '\n——————————————————————————————'+
-          '\n Order ID  : '+ result.insertId+" "+" No."+req.user.numberOfOrders+1+
+          '\n Order ID  : '+'32'+ result0.insertId+'47'+" "+" No."+numOfOrders+
           '\n Date       : '+ req.body.date+
           '\n Time       : '+ req.body.time+
           '\n Duration  : '+ req.body.duration+
@@ -79,7 +79,7 @@ router.post('/orderPreview', checkAuthentication.checkAuthenticated,  (req, res)
           '\n——————————————————————————————'+
           '\n User ID     : '+ req.user.user_id+
           '\n Name       : '+ req.user.firstname+" "+req.user.lastname+
-          '\n Email      : '+ req.user.email+
+          '\n Email        : '+ req.user.email+
           '\n Telephone : '+ req.user.telephone+
           '\n Address    : '+ req.user.address+
           '\n Floor        : '+ req.user.floor+
@@ -88,6 +88,41 @@ router.post('/orderPreview', checkAuthentication.checkAuthenticated,  (req, res)
       };
 
       smtpTrans.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      var mailOptions1 = {
+        from: 'sender',
+        to: req.user.email, //shishahublc@gmail.com //sipmarch7@hotmail.com
+        subject: 'Your Shishahub Order '+req.user.firstname,
+        text: 'Ευχαριστούμε για την παραγγελία σου στο Shishahub.'+
+          '\nΣύντομα θα ακολουθήσει email επιβεβαίωσης και αποδοχής της παραγγελίας σου.'+
+          '\nΠαρακάτω υπάρχουν τα στοιχεία της παραγγελίας σου.'+
+          '\nΓια οποιαδήποτε αλλαγή επικοινώνησε μαζί μας στο DM του instagram @shishahub.lc ή στο email εδώ.'+
+          '\n\nORDER DETAILS'+
+          '\n——————————————————————————————'+
+          '\n Order ID  : '+'32'+ result0.insertId+'47'+" "+" No."+numOfOrders+
+          '\n Date       : '+ req.body.date+
+          '\n Time       : '+ req.body.time+
+          '\n Duration  : '+ req.body.duration+
+          '\n Flavors    : '+ req.body.flavors+
+          '\n Price      : '+ price(req.body.duration, req.user.outOfLoutraki, req.user.numberOfOrders)+
+          '\n Other Address : '+ req.body.textArea+
+          '\n\nACCOUNT DETAILS'+
+          '\n——————————————————————————————'+
+          '\n Name       : '+ req.user.firstname+" "+req.user.lastname+
+          '\n Email        : '+ req.user.email+
+          '\n Telephone : '+ req.user.telephone+
+          '\n Address    : '+ req.user.address+
+          '\n Floor        : '+ req.user.floor+
+          '\n City         : '+ req.user.city+
+          '\n Postal      : '+ req.user.postal
+      };
+
+      smtpTrans.sendMail(mailOptions1, function(error, info){
         if (error) {
           console.log(error);
         } else {
