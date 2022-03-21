d4f6bd226521d4699702c75c0f14e73ebb251a06
sipmarch7 <sipmarch7@hotmail.com>
2022-03-21 20:38:49 +0200

    add docker

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ const adminRouter = require('./routes/admin');
 app.use('/admin', adminRouter);
 
 //port number
-var portNumber = process.env.port || process.env.PORT || 3006;
+var portNumber = process.env.PORT || 3010;
 //server listening
 app.listen(portNumber, () => {
   console.log('Server is running at port '+portNumber);
